f27caf186991e5d9d4a67e45ae97243140ef7e6a
Osama El Henaweey <87882318+osama-elhenaweey@users.noreply.github.com>
2023-09-15 03:08:43 +0300

    ✅Adding Some JS .✅

--- script.js
+++ script.js
@@ -93,3 +93,49 @@ tabsContainer.addEventListener("click", function (e) {
         .querySelector(`.operations__content--${o}`)
         .classList.add("operations__content--active");
 });
+
+// nav  fading animation
+
+const navfun = function (e, opacity) {
+    if (e.target.classList.contains("nav__link")) {
+        const link = e.target;
+        const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+        const logo = link.closest(".nav").querySelector(".nav__logo");
+        siblings.forEach((el) => {
+            if (el != link) {
+                el.style.opacity = opacity;
+            }
+        });
+        logo.style.opacity = opacity;
+    }
+};
+
+const nav = document.querySelector(".nav");
+nav.addEventListener("mouseover", function (e) {
+    // if (e.target.classList.contains("nav__link")) {
+    //     const link = e.target;
+    //     const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    //     const logo = link.closest(".nav").querySelector(".nav__logo");
+    //     siblings.forEach((el) => {
+    //         if (el != link) {
+    //             el.style.opacity = "0.5";
+    //         }
+    //     });
+    //     logo.style.opacity = "0.5";
+    // }
+    navfun(e, 0.5);
+});
+nav.addEventListener("mouseout", function (e) {
+    // if (e.target.classList.contains("nav__link")) {
+    //     const link = e.target;
+    //     const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    //     const logo = link.closest(".nav").querySelector(".nav__logo");
+    //     siblings.forEach((el) => {
+    //         if (el != link) {
+    //             el.style.opacity = "1";
+    //         }
+    //     });
+    //     logo.style.opacity = "1";
+    // }
+    navfun(e, 1);
+});
